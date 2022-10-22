b5d7a6117dc29b185c04334f08c6bc5b938b49ba
adistianjani <adistianjaniputri90@gmail.com>
2022-10-22 10:14:52 +0800

    + slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,105 +1,122 @@
 (function($) {
-  
-  "use strict";  
 
-  $(window).on('load', function() {
+    "use strict";
 
-  /*Page Loader active
-  ========================================================*/
-  $('#preloader').fadeOut();
+    $(window).on('load', function() {
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 50) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
-    });
+        /*Page Loader active
+        ========================================================*/
+        $('#preloader').fadeOut();
 
-    // one page navigation 
-    $('.navbar-nav').onePageNav({
-      currentClass: 'active'
-    });
+        // Sticky Nav
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 50) {
+                $('.scrolling-navbar').addClass('top-nav-collapse');
+            } else {
+                $('.scrolling-navbar').removeClass('top-nav-collapse');
+            }
+        });
 
-    /* Auto Close Responsive Navbar on Click
-    ========================================================*/
-    function close_toggle() {
-        if ($(window).width() <= 768) {
-            $('.navbar-collapse a').on('click', function () {
-                $('.navbar-collapse').collapse('hide');
-            });
-        }
-        else {
-            $('.navbar .navbar-inverse a').off('click');
-        }
-    }
-    close_toggle();
-    $(window).resize(close_toggle);
+        // one page navigation 
+        $('.navbar-nav').onePageNav({
+            currentClass: 'active'
+        });
 
-    /* WOW Scroll Spy
-    ========================================================*/
-     var wow = new WOW({
-      //disabled for mobile
-        mobile: false
-    });
-
-    wow.init();    
-
-     /* Testimonials Carousel 
-    ========================================================*/
-    var owl = $("#testimonials");
-      owl.owlCarousel({
-        loop: true,
-        nav: false,
-        dots: true,
-        center: true,
-        margin: 15,
-        slideSpeed: 1000,
-        stopOnHover: true,
-        autoPlay: true,
-        responsiveClass: true,
-        responsiveRefreshRate: true,
-        responsive : {
-            0 : {
-                items: 1
-            },
-            768 : {
-                items: 2
-            },
-            960 : {
-                items: 3
-            },
-            1200 : {
-                items: 3
-            },
-            1920 : {
-                items: 3
+        /* Auto Close Responsive Navbar on Click
+        ========================================================*/
+        function close_toggle() {
+            if ($(window).width() <= 768) {
+                $('.navbar-collapse a').on('click', function() {
+                    $('.navbar-collapse').collapse('hide');
+                });
+            } else {
+                $('.navbar .navbar-inverse a').off('click');
             }
         }
-      });  
-      
+        close_toggle();
+        $(window).resize(close_toggle);
 
-    /* Back Top Link active
+        /* WOW Scroll Spy
+        ========================================================*/
+        var wow = new WOW({
+            //disabled for mobile
+            mobile: false
+        });
+
+        wow.init();
+
+        /* Testimonials Carousel 
     ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+        var owl = $("#testimonials");
+        owl.owlCarousel({
+            loop: true,
+            nav: false,
+            dots: true,
+            center: true,
+            margin: 15,
+            slideSpeed: 1000,
+            stopOnHover: true,
+            autoPlay: true,
+            responsiveClass: true,
+            responsiveRefreshRate: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                960: {
+                    items: 3
+                },
+                1200: {
+                    items: 3
+                },
+                1920: {
+                    items: 3
+                }
+            }
+        });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
 
-  });      
+        /* Back Top Link active
+        ========================================================*/
+        var offset = 200;
+        var duration = 500;
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > offset) {
+                $('.back-to-top').fadeIn(400);
+            } else {
+                $('.back-to-top').fadeOut(400);
+            }
+        });
+
+        $('.back-to-top').on('click', function(event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
+
+    });
 
 }(jQuery));
+
+//-----------
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
