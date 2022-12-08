c95b6f3003ab5a3e0afd88073f31fcb175120a6f
rahelkristina <rahelprajnyawati@gmail.com>
2022-12-08 13:11:47 +0700

    chore: enhance lp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,106 +1,231 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="assets/img/logofishku.png">
+    <title>Fi$hku Indonesia | Berdayakan Perikanan Indonesia Mewujudkan Keadilan Sosial Bagi Seluruh Rakyat Indonesia</title>
 
-    <title>{{ config('app.name', 'Fi$hku') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
-
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <!-- Icon -->
-    <link href="{{ asset('assets/fonts/line-icons.css') }} " rel="stylesheet">
-
+    <link rel="stylesheet" href="assets/fonts/line-icons.css">
     <!-- Owl carousel -->
-    <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/owl.theme.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.css">
 
     <!-- Animate -->
-    <link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet">
-
+    <link rel="stylesheet" href="assets/css/animate.css">
     <!-- Main Style -->
-    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
-    
+    <link rel="stylesheet" href="assets/css/main.css">
     <!-- Responsive Style -->
-    <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet">
-
-     
-
-    @livewireStyles
+    <link rel="stylesheet" href="assets/css/responsive.css">
 </head>
 <body>
-    <!-- <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    
+<header id="header-wrap">
+        <!-- Navbar Start -->
+        <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <a href="index.html" class="navbar-brand"><img src="assets/img/logo-fishku.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="lni-menu"></i>
                 </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto w-100 justify-content-end clearfix">
+                        <!-- HOME -->
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#hero-area">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#services">
+                                Features
+                            </a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="#news">
+                                News
+                            </a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#testimonial">
+                                Documentation
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#team">
+                                Team
+                            </a>
+                        </li>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent"> -->
-                    <!-- Left Side Of Navbar -->
-                    <!-- <ul class="navbar-nav me-auto">
 
-                    </ul> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#footer">
+                                Contact
+                            </a>
+                        </li>
 
-                    <!-- Right Side Of Navbar -->
-                    <!-- <ul class="navbar-nav ms-auto"> -->
-                        <!-- Authentication Links -->
-                        <!-- @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest -->
-                    <!-- </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="privacy.html">
+                                Privacy Policy
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </nav> -->
+        </nav>
 
-        <!-- <main class="py-4">
+        <main class="py-4">
             @yield('content')
         </main>
-    </div> -->
 
-    <!-- Scripts -->
-    <script href="{{ asset('assets/js/jquery-3.6.1.min.js') }}" defer></script>
-    <script href="{{ asset('assets/js/bootstrap.bundle.min.js') }}" defer></script>
+        <footer id="footer" class="footer-area section-padding">
+        <div class="container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+                        <div class="widget">
+                            <h3 class="footer-logo"><img src="assets/img/logofishku.png" width="70px" alt=""></h3>
+                            <div class="textwidget">
+                                <p>Fishku merupakan startup e-commerce di bidang perikanan yang bertujuan untuk mempermudah kegiatan jual beli ikan dan mendeteksi kesegaran ikan menggunakan machine learning.</p>
+                            </div>
 
-    @livewireScripts
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                        <h3 class="footer-titel">Supported by</h3>
+                        <ul class="support">
+                            <li><img src="./assets/img/footer/bangkit.jpg" width="70px">
+                            </li>
+                            <li><img src="./assets/img/footer/google.png" width="90px">
+                            </li>
+                            <li><img src="./assets/img/footer/dikti.png" width="70px">
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                        <h3 class="footer-titel">Contact</h3>
+                        <ul class="address">
+                            <li>
+                                <a href="https://goo.gl/maps/e4qz8f8EDFi3Shmf6"><i class="lni-map-marker"></i> Universitas Gajah Mada - Daerah Istimewa Yogyakarta </a>
+                            </li>
+                            <!-- <li>
+                                <a href="#"><i class="lni-phone-handset"></i> P: -</a>
+                            </li> -->
+                            <li>
+                                <a href=mailto:admin@fishku.id?subject="HTML link"><i class="lni-envelope"></i>admin@fishku.id</a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/fishku.id/"><i class="lni-instagram"></i>fishku.id</a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/company/fishku/"><i class="lni-linkedin"></i>Fishku Indonesia</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        <div id="copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="copyright-content">
+                            <p>Copyright © 2022 <a rel="nofollow" href="https://fishku.id">Fi$hku</a> All Right Reserved
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    <!-- Go to Top Link -->
+    <a href="#" class="back-to-top">
+        <i class="lni-arrow-up"></i>
+    </a>
+
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="loader" id="loader-1"></div>
+    </div>
+    <!-- End Preloader -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/jquery.nav.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/form-validator.min.js"></script>
+    <script src="assets/js/contact-form-script.min.js"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.1/iconify-icon.min.js"></script>
+    <script>
+        $('.team-slider').owlCarousel({
+            loop: true,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 450,
+            margin: -105,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                991: {
+                    items: 3
+                },
+                1200: {
+                    items: 3
+                },
+                1920: {
+                    items: 3
+                }
+            }
+        });
+    </script>
+    <script>
+        $('.testi').owlCarousel({
+            loop: true,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 450,
+            margin: -105,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                991: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        });
+    </script>
 </body>
 </html>
