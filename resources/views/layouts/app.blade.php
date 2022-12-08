635d5b3702a39683c61810d1b2d93ac3d5eb9b84
rahelkristina <rahelprajnyawati@gmail.com>
2022-12-08 11:48:59 +0700

    chore: add styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- Styles -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
@@ -76,5 +77,11 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- Scripts -->
+    <script href="{{ asset('assets/js/jquery-3.6.1.min.js') }}" defer></script>
+    <script href="{{ asset('assets/js/bootstrap.bundle.min.js') }}" defer></script>
+
+
 </body>
 </html>
