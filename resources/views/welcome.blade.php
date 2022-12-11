903e3fa0daeccee02c8be6340e3cd3783c11601c
rahelkristina <rahelprajnyawati@gmail.com>
2022-12-11 14:02:45 +0700

    fix: height funfact

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,19 +88,19 @@
 				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
 			</div>
 			<div class="carousel-inner">
-				<div class="carousel-item active"> <img src="assets/img/funfact/croppednatunas.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-item active"> <img src="assets/img/funfact/croppednatunas.jpg" class="d-block w-100 h-funfact" alt="...">
 					<div class="carousel-caption d-none d-md-block">
 						<h5>First slide label</h5>
 						<p>Some representative placeholder content for the first slide.</p>
 					</div>
 				</div>
-				<div class="carousel-item"> <img src="assets/img/funfact/tunaocean.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-item"> <img src="assets/img/funfact/tunaocean.jpg" class="d-block w-100 h-funfact" alt="...">
 					<div class="carousel-caption d-none d-md-block">
 						<h5>Second slide label</h5>
 						<p>Some representative placeholder content for the second slide.</p>
 					</div>
 				</div>
-				<div class="carousel-item"> <img src="assets/img/funfact/sea.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-item"> <img src="assets/img/funfact/sea.jpg" class="d-block w-100 h-funfact" alt="...">
 					<div class="carousel-caption d-none d-md-block">
 						<h5>Third slide label</h5>
 						<p>Some representative placeholder content for the third slide.</p>
