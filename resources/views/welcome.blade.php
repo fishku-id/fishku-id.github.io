ce73476c8da3ba4909f91a18c04568647123c684
rahelkristina <rahelprajnyawati@gmail.com>
2022-12-12 08:31:26 +0700

    fix: testimonial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -286,18 +286,11 @@
                     <img src="assets/img/testimonial/img1.jpg" alt="">
                   </div>
                   <div class="info">
-                    <h2><a href="#">David Smith</a></h2>
-                    <h3><a href="#">Creative Head</a></h3>
+                    <h2><a href="">David Smith</a></h2>
+                    <h3><a href="">Creative Head</a></h3>
                   </div>
                   <div class="content">
                     <p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
-                    <div class="star-icon mt-3">
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-half"></i></span>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -307,18 +300,11 @@
                     <img src="assets/img/testimonial/img2.jpg" alt="">
                   </div>
                   <div class="info">
-                    <h2><a href="#">Domeni GEsson</a></h2>
-                    <h3><a href="#">Awesome Technology co.</a></h3>
+                    <h2><a href="">Domeni GEsson</a></h2>
+                    <h3><a href="">Awesome Technology co.</a></h3>
                   </div>
                   <div class="content">
                     <p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
-                    <div class="star-icon mt-3">
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-half"></i></span>
-                      <span><i class="lni-star-half"></i></span>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -328,18 +314,11 @@
                     <img src="assets/img/testimonial/img3.jpg" alt="">
                   </div>
                   <div class="info">
-                    <h2><a href="#">Dommini Albert</a></h2>
-                    <h3><a href="#">Nesnal Design co.</a></h3>
+                    <h2><a href="">Dommini Albert</a></h2>
+                    <h3><a href="">Nesnal Design co.</a></h3>
                   </div>
                   <div class="content">
                     <p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
-                    <div class="star-icon mt-3">
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-half"></i></span>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -349,18 +328,11 @@
                     <img src="assets/img/testimonial/img4.jpg" alt="">
                   </div>
                   <div class="info">
-                    <h2><a href="#">Fernanda Anaya</a></h2>
-                    <h3><a href="#">Developer</a></h3>
+                    <h2><a href="">Fernanda Anaya</a></h2>
+                    <h3><a href="">Developer</a></h3>
                   </div>
                   <div class="content">
                     <p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
-                    <div class="star-icon mt-3">
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-filled"></i></span>
-                      <span><i class="lni-star-half"></i></span>
-                      <span><i class="lni-star-half"></i></span>
-                      <span><i class="lni-star-half"></i></span>
-                    </div>
                   </div>
                 </div>
               </div>
