27533afe4dff0d5f3539c3fb39e52941df94ed14
rahelkristina <rahelprajnyawati@gmail.com>
2022-12-15 08:44:40 +0700

    chore: add html funfact carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,13 @@
 						<p>Digitalisasi penjualan ikan merupakan salah satu alternatif untuk memudahkan penjualan ikan. Beberapa fitur seperti
 							<br>jual beli ikan dan deteksi kesegaran hadir di aplikasi ini.
 							<br>Ayo unduh sekarang!</p>
-						<div class="header-button"> <a href="https://play.google.com/store/apps/details?id=id.fishku.fisherseller" class="btn btn-common">Fishku Partner</i>
+						<!-- <div class="header-button"> <a href="https://play.google.com/store/apps/details?id=id.fishku.fisherseller" class="btn btn-common">Fishku Partner</i>
                                 </a> <a href="https://play.google.com/store/apps/details?id=id.fishku.consumer" class="btn btn-common">Fishku Consumer</i>
-                                </a> </div>
+                                </a>  -->
+						<!-- </div> -->
+						<div class="header-button"> <a href="https://play.google.com/store/apps/details?id=id.fishku.fisherseller" class="btn btn-common">Find us on Instagram</i>
+                                </a>
+						</div>
 					</div>
 				</div>
 				<div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
@@ -140,6 +144,58 @@
         </div> -->
 </section>
 <!-- News Section End -->
+
+<div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="mask flex-center">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-7 col-12 order-md-1 order-2">
+              <h4>Present your <br>
+                awesome product</h4>
+              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+              <a href="#">BUY NOW</a> </div>
+            <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/NKvkfTT.png" class="mx-auto" alt="slide"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="mask flex-center">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-7 col-12 order-md-1 order-2">
+              <h4>Present your <br>
+                awesome product</h4>
+              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+              <a href="#">BUY NOW</a> </div>
+            <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/duWgXRs.png" class="mx-auto" alt="slide"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="mask flex-center">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-7 col-12 order-md-1 order-2">
+              <h4>Present your <br>
+                awesome product</h4>
+              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
+                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+              <a href="#">BUY NOW</a> </div>
+            <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/DGkR4OQ.png" class="mx-auto" alt="slide"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+<!--slide end--> 
+
 <!-- Team slider Start -->
 <section id="team">
 	<div class="container">
