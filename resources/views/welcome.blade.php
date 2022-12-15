7d77a6a86405873813efb2ccc6093630180f8730
rahelkristina <rahelprajnyawati@gmail.com>
2022-12-15 11:50:18 +0700

    fix: download section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
                                 </a> <a href="https://play.google.com/store/apps/details?id=id.fishku.consumer" class="btn btn-common">Fishku Consumer</i>
                                 </a>  -->
 						<!-- </div> -->
-						<div class="header-button"> <a href="https://play.google.com/store/apps/details?id=id.fishku.fisherseller" class="btn btn-common">Find us on Instagram</i>
+						<div class="header-button"> <a href="https://instagram.com/fishku.id" class="btn btn-common">Find us on Instagram</i>
                                 </a> </div>
 					</div>
 				</div>
@@ -136,7 +136,7 @@
 	</div>
 </section>
 <!--slide end-->
-<!-- funfact end -->
+
 <!-- Team slider Start -->
 <section id="team">
 	<div class="container">
@@ -237,20 +237,47 @@
 </section>
 <!-- download section -->
 <section>
-	<div class="container">
+<div class="container">
 		<div class="row">
-			<div class="col-lg-12">
-				<div>
-					<h4>
-					Test
-				</h4> </div>
-				<div>
-					<h4>
-					Test
-				</h4> </div>
+			<div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
 			</div>
 		</div>
+	</div>
+	<div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<div class="mask flex-center">
+					<div class="container">
+						<div class="row align-items-center" style="margin-top: 30px">
+						<div class="col-md-7 col-12 order-md-1 order-2"><img src="assets/img/funfact/croppednatunas.jpg" class="mx-auto" alt="slide" style="height: 225px; width: 400px"></div>
+							<div class="col-md-5 col-12 order-md-2 order-1">
+								<h3>
+									FI$HKU TELAH HADIR DI <br>
+									APP STORE DAN PLAY STORE
+								</h3>
+								<p>Bersama Fishku mari kita <br>
+									#CiptakanPasarmu #DiFishkuPastiSegar <br>
+									#SemuaMakanIkan</p>
+									<div class="header-button">
+
+									<a href="" target="_blank" rel="noreferrer">
+									<img alt="ica" src="assets/img/icon/icon-ios.webp" class="icon-download css-2gb014 " style="height:50px" draggable="false">
+                                </a>
+
+                                <a href="https://play.google.com/store/apps/details?id=id.fishku.consumer" target="_blank" rel="noreferrer">
+									<img alt="ica" src="assets/img/icon/icon-gplay.webp" class="icon-download css-2gb014" style="height:50px" draggable="false">
+                                </a>
+                            </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
+
+
 <!-- end of download section -->
 <section id="testimonial" class="testimonial section-padding">
 	<div style="margin: 30px 60px">
