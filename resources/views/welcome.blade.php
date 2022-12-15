6e6618fcef2c85cb26097485d77ccb19f0b5dbd2
rahelkristina <rahelprajnyawati@gmail.com>
2022-12-15 10:44:32 +0700

    fix: funfact

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,7 @@
                                 </a>  -->
 						<!-- </div> -->
 						<div class="header-button"> <a href="https://play.google.com/store/apps/details?id=id.fishku.fisherseller" class="btn btn-common">Find us on Instagram</i>
-                                </a>
-						</div>
+                                </a> </div>
 					</div>
 				</div>
 				<div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
@@ -40,8 +39,7 @@
 			<!-- Services item -->
 			<div class="col-md-4 col-lg-4 col-xs-12">
 				<div class="services-item wow fadeInRight" data-wow-delay="0.3s">
-					<div class="icon">
-						<img src="assets/img/feature/consumer-removebg-preview.png" width="120">
+					<div class="icon"> <img src="assets/img/feature/consumer-removebg-preview.png" width="120">
 						<!-- <iconify-icon icon="ant-design:shopping-cart-outlined" style="color: white;" width="50" height="50"></iconify-icon> -->
 					</div>
 					<div class="services-content">
@@ -53,8 +51,7 @@
 			<!-- Services item -->
 			<div class="col-md-4 col-lg-4 col-xs-12">
 				<div class="services-item wow fadeInRight" data-wow-delay="0.6s">
-					<div class="icon">
-					<img src="assets/img/feature/seller-removebg-preview.png" width="120">
+					<div class="icon"> <img src="assets/img/feature/seller-removebg-preview.png" width="120">
 						<!-- <iconify-icon icon="bx:store" style="color: white;" width="50" height="50"></iconify-icon> -->
 					</div>
 					<div class="services-content">
@@ -66,8 +63,7 @@
 			<!-- Services item -->
 			<div class="col-md-4 col-lg-4 col-xs-12">
 				<div class="services-item wow fadeInRight" data-wow-delay="0.9s">
-					<div class="icon">
-					<img src="assets/img/feature/deteksi-removebg-preview.png" width="120">
+					<div class="icon"> <img src="assets/img/feature/deteksi-removebg-preview.png" width="120">
 						<!-- <iconify-icon icon="clarity:fish-line" style="color: white;" width="50" height="50"></iconify-icon> -->
 					</div>
 					<div class="services-content">
@@ -79,105 +75,68 @@
 		</div>
 	</div>
 </section>
-<!-- Services Section End -->
-<!-- News Section -->
-
-		<!-- carousel -->
-		<!-- <div class="row"> -->
-		<!-- Services item -->
-		<!-- <div class="col-md-4 col-lg-5 col-xs-12">
-                    <div class="services-item wow fadeInRight" data-wow-delay="0.3s"> -->
-		<!-- <div class="icon">
-                            <iconify-icon icon="ant-design:shopping-cart-outlined" style="color: white;" width="50" height="50"></iconify-icon>
-                        </div> -->
-		<!-- <div class="services-content">
-                            <p>Gambarnya Disini</p>
-                        </div>
-                    </div>
-                </div> -->
-		<!-- Services item -->
-		<!-- <div class="col-md-4 col-lg-7 col-xs-12">
-                    <div class="services-item wow fadeInRight" data-wow-delay="0.6s"> -->
-		<!-- <div class="icon">
-                            <iconify-icon icon="bx:store" style="color: white;" width="50" height="50"></iconify-icon>
-                        </div> -->
-		<!-- <div class="news-content"> -->
-		<!-- <h6>Apa Itu Ekonomi Biru???</h6> -->
-		<!-- <p>Menurut Bank Dunia, ekonomi biru adalah “pemanfaatan sumber daya laut yang berwawasan lingkungan untuk mendukung pertumbuhan ekonomi, kesejahteraan, dan mata pencaharian sekaligus pelestarian ekosistem laut”.</p> -->
-		<!-- <p>Sumber: BKPM</p> -->
-		<!-- </div>
-                    </div>
-                </div> -->
-		<!-- </div>
-        </div> -->
-</section>
 <!-- News Section End -->
 
 <!-- funfact session -->
 <section>
-<div class="container">
+	<div class="container">
 		<div class="row">
 			<div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
 				<div class="sec-heading text-center">
-					<h2>Fun Fact</h2> 
+					<h2>Fun Fact</h2>
 					<p>Fishitors tau gasih?</p>
 				</div>
 			</div>
 		</div>
 	</div>
-<div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <div class="mask flex-center">
-        <div class="container">
-          <div class="row align-items-center" style="margin-top: 30px">
-            <div class="col-md-7 col-12 order-md-1 order-2">
-              <h4>Present your <br>
-                awesome product</h4>
-              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
-                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
-             </div>
-            <div class="col-md-5 col-12 order-md-2 order-1"><img src="assets/img/funfact/croppednatunas.jpg" class="mx-auto" alt="slide" style="height: 225px; width: 400px"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <div class="mask flex-center">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-md-7 col-12 order-md-1 order-2">
-              <h4>Present your <br>
-                awesome product</h4>
-              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
-                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
-               </div>
-            <div class="col-md-5 col-12 order-md-2 order-1"><img src="assets/img/funfact/tunaocean.jpg" class="mx-auto" alt="slide" style="height: 225px; width: 400px"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <div class="mask flex-center">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-md-7 col-12 order-md-1 order-2">
-              <h4>Present your <br>
-                awesome product</h4>
-              <p>Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum <br>
-                necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
-              </div>
-            <div class="col-md-5 col-12 order-md-2 order-1"><img src="assets/img/funfact/sea.jpg" class="mx-auto" alt="slide" style="height: 225px; width: 400px"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+	<div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<div class="mask flex-center">
+					<div class="container">
+						<div class="row align-items-center" style="margin-top: 30px">
+							<div class="col-md-7 col-12 order-md-1 order-2">
+								<p style="margin-left: 70px">Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum
+									<br> necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+							</div>
+							<div class="col-md-5 col-12 order-md-2 order-1"><img src="assets/img/funfact/croppednatunas.jpg" class="mx-auto" alt="slide" style="height: 225px; width: 400px"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="mask flex-center">
+					<div class="container">
+						<div class="row align-items-center" style="margin-top: 30px">
+							<div class="col-md-7 col-12 order-md-1 order-2">
+								<p style="margin-left: 70px">Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum
+									<br> necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+							</div>
+							<div class="col-md-5 col-12 order-md-2 order-1"><img src="assets/img/funfact/tunaocean.jpg" class="mx-auto" alt="slide" style="height: 225px; width: 400px"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="mask flex-center">
+					<div class="container">
+						<div class="row align-items-center" style="margin-top: 30px">
+							<div class="col-md-7 col-12 order-md-1 order-2">
+								<p style="margin-left: 70px">Lorem ipsum dolor sit amet. Reprehenderit, qui blanditiis quidem rerum
+									<br> necessitatibus praesentium voluptatum deleniti atque corrupti.</p>
+							</div>
+							<div class="col-md-5 col-12 order-md-2 order-1"><img src="assets/img/funfact/sea.jpg" class="mx-auto" alt="slide" style="height: 225px; width: 400px"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only" style="font-size: 30px">🡸</span> </a>
+		<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only" style="font-size: 30px">🡺</span> </a>
+	</div>
 </section>
-<!--slide end--> 
+<!--slide end-->
 <!-- funfact end -->
-
 <!-- Team slider Start -->
 <section id="team">
 	<div class="container">
@@ -276,113 +235,78 @@
 		</div>
 	</div>
 </section>
-<!-- Download Section -->
-<!-- <section id="news">
-        <div class="container">
-            <div class="sec-heading text-center">
-                <h2> FI$HKU AKAN HADIR DI APP STORE DAN PLAY STORE</h2> -->
-<!-- <div class="shape wow fadeInDown" data-wow-delay="0.3s"></div> -->
-<!-- <h5>Bersama Fishku mari kita
-                        #CiptakanPasarmu #DiFishkuPastiSegar #SemuaMakanIkan</h5>
-            </div>
-            <div class="row"> -->
-<!-- Services item -->
-<!-- <div class="col-md-4 col-lg-5 col-xs-12">
-                    <div class="services-item wow fadeInRight" data-wow-delay="0.3s"> -->
-<!-- <div class="icon">
-                            <iconify-icon icon="ant-design:shopping-cart-outlined" style="color: white;" width="50" height="50"></iconify-icon>
-                        </div> -->
-<!-- <div class="services-content">
-                            <p>Gambarnya Disini</p>
-                        </div>
-                    </div>
-                </div> -->
-<!-- Services item -->
-<!-- <div class="col-md-4 col-lg-7 col-xs-12">
-                    <div class="services-item wow fadeInRight" data-wow-delay="0.6s"> -->
-<!-- <div class="icon">
-                            <iconify-icon icon="bx:store" style="color: white;" width="50" height="50"></iconify-icon>
-                        </div> -->
-<!-- <div class="news-content">
-                            <h6>Apa Itu Ekonomi Biru???</h6>
-                            <p>Menurut Bank Dunia, ekonomi biru adalah “pemanfaatan sumber daya laut yang berwawasan lingkungan untuk mendukung pertumbuhan ekonomi, kesejahteraan, dan mata pencaharian sekaligus pelestarian ekosistem laut”.</p>
-                            <p>Sumber: BKPM</p>
-                        </div>
-                    </div>
-                </div> -->
-<!-- </div>
-        </div>
-    </section> -->
-<!-- Download Section End -->
-<!-- Testimonial Section Start -->
+<!-- download section -->
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div>
+					<h4>
+					Test
+				</h4> </div>
+				<div>
+					<h4>
+					Test
+				</h4> </div>
+			</div>
+		</div>
+</section>
+<!-- end of download section -->
 <section id="testimonial" class="testimonial section-padding">
 	<div style="margin: 30px 60px">
-<h-testimonial>   Kata Mereka...</h-testimonial>
-</div>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div id="testimonials" class="owl-carousel wow fadeInUp" data-wow-delay="1.2s">
-              <div class="item">
-                <div class="testimonial-item">
-                  <div class="img-thumb">
-                    <img src="assets/img/testimonial/img1.jpg" alt="">
-                  </div>
-                  <div class="info">
-                    <h2><a href="">David Smith</a></h2>
-                    <h3><a href="">Creative Head</a></h3>
-                  </div>
-                  <div class="content">
-                    <p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="testimonial-item">
-                  <div class="img-thumb">
-                    <img src="assets/img/testimonial/img2.jpg" alt="">
-                  </div>
-                  <div class="info">
-                    <h2><a href="">Domeni GEsson</a></h2>
-                    <h3><a href="">Awesome Technology co.</a></h3>
-                  </div>
-                  <div class="content">
-                    <p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="testimonial-item">
-                  <div class="img-thumb">
-                    <img src="assets/img/testimonial/img3.jpg" alt="">
-                  </div>
-                  <div class="info">
-                    <h2><a href="">Dommini Albert</a></h2>
-                    <h3><a href="">Nesnal Design co.</a></h3>
-                  </div>
-                  <div class="content">
-                    <p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
-                  </div>
-                </div>
-              </div>
-              <div class="item">
-                <div class="testimonial-item">
-                  <div class="img-thumb">
-                    <img src="assets/img/testimonial/img4.jpg" alt="">
-                  </div>
-                  <div class="info">
-                    <h2><a href="">Fernanda Anaya</a></h2>
-                    <h3><a href="">Developer</a></h3>
-                  </div>
-                  <div class="content">
-                    <p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- Testimonial Section End -->
-@endsection
+		<h-testimonial> Kata Mereka...</h-testimonial>
+	</div>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div id="testimonials" class="owl-carousel wow fadeInUp" data-wow-delay="1.2s">
+					<div class="item">
+						<div class="testimonial-item">
+							<div class="img-thumb"> <img src="assets/img/testimonial/img1.jpg" alt=""> </div>
+							<div class="info">
+								<h2><a href="">David Smith</a></h2>
+								<h3><a href="">Creative Head</a></h3> </div>
+							<div class="content">
+								<p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="testimonial-item">
+							<div class="img-thumb"> <img src="assets/img/testimonial/img2.jpg" alt=""> </div>
+							<div class="info">
+								<h2><a href="">Domeni GEsson</a></h2>
+								<h3><a href="">Awesome Technology co.</a></h3> </div>
+							<div class="content">
+								<p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="testimonial-item">
+							<div class="img-thumb"> <img src="assets/img/testimonial/img3.jpg" alt=""> </div>
+							<div class="info">
+								<h2><a href="">Dommini Albert</a></h2>
+								<h3><a href="">Nesnal Design co.</a></h3> </div>
+							<div class="content">
+								<p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="testimonial-item">
+							<div class="img-thumb"> <img src="assets/img/testimonial/img4.jpg" alt=""> </div>
+							<div class="info">
+								<h2><a href="">Fernanda Anaya</a></h2>
+								<h3><a href="">Developer</a></h3> </div>
+							<div class="content">
+								<p class="description">Praesent cursus nulla non arcu tempor, ut egestas elit tempus. In ac ex fermentum, gravida felis nec, tincidunt ligula.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Testimonial Section End -->@endsection
